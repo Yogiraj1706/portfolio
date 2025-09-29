48218368ddf887df8f5617309b33f3280316ed14
Yogiraj Nimbalkar <yogirajnimbalkar17@gmail.com>
2025-09-29 15:52:48 +0530

    added resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -10,7 +10,7 @@ function Resume() {
         Here’s my professional journey, showcasing my skills, experience, and achievements as a Java developer. Download my resume to explore my career path and technical expertise in detail. You can download it by clicking the button below.
       </p>
       <a
-        href="/YogirajNimbalkar_Resume.pdf" // This is the path to your resume file in the public folder
+        href="/Yogiraj_Nimbalkar_Resume.pdf" // This is the path to your resume file in the public folder
         download="Yogiraj_Nimbalkar_Resume"   // This attribute triggers the download and specifies the filename
         className="download-btn"
       >
